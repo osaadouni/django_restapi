a59203cf2c29e4339bd911b82ba0bb2567adab78
Omar Saadouni <os@amstelnet.com>
2019-01-07 20:01:13 +0100

    changes commited

--- snippets/views.py
+++ snippets/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.models import User
+
 from rest_framework import generics
+from rest_framework import permissions
 
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer 
+from snippets.permissions import IsOwnerOrReadOnly
 
 
 
@@ -12,6 +16,10 @@ class SnippetList(generics.ListCreateAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -20,4 +28,15 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
   
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
